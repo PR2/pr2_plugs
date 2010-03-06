0cb29a48d71911d6c474d99ff02cec47229846d0
mwise <devnull@localhost>
2010-03-06 00:32:52 +0000

    removing action generator in preference for action tool

--- pr2_plugs_actions/scripts/detect_wall_norm.py
+++ pr2_plugs_actions/scripts/detect_wall_norm.py
@@ -1,37 +1,37 @@
-#!/usr/bin/env python                                                                        
-# Software License Agreement (BSD License)                                                   
-#                                                                                            
-# Copyright (c) 2009, Willow Garage, Inc.                                                    
-# All rights reserved.                                                                       
-#                                                                                            
-# Redistribution and use in source and binary forms, with or without                         
-# modification, are permitted provided that the following conditions                         
-# are met:                                                                                   
-#                                                                                            
-#  * Redistributions of source code must retain the above copyright                          
-#    notice, this list of conditions and the following disclaimer.                           
-#  * Redistributions in binary form must reproduce the above                                 
-#    copyright notice, this list of conditions and the following                             
-#    disclaimer in the documentation and/or other materials provided                         
-#    with the distribution.                                                                  
-#  * Neither the name of the Willow Garage nor the names of its                              
-#    contributors may be used to endorse or promote products derived                         
-#    from this software without specific prior written permission.                           
-#                                                                                            
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS                        
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT                          
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS                          
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE                             
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                        
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,                       
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;                           
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER                           
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT                         
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                          
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE                            
-# POSSIBILITY OF SUCH DAMAGE.                                                                
-#                                                                                            
-# Author Melonee Wise                                                    
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2009, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the Willow Garage nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author Melonee Wise
 
 
 import roslib; roslib.load_manifest('pr2_plugs_actions')
@@ -51,26 +51,26 @@ class DetectWallNormServer:
   def __init__(self, name):
     self.name = name
     self.sim = rospy.get_param('~sim', False)
-    
+
     self.head_client = actionlib.SimpleActionClient('head_traj_controller/point_head_action', PointHeadAction)
     self.head_client.wait_for_server()
 
     rospy.wait_for_service('stereo_wall_detection/detect_wall')
     rospy.loginfo('detect wall service found')
-    self.detect_wall_srv = rospy.ServiceProxy('stereo_wall_detection/detect_wall', DetectWall) 
+    self.detect_wall_srv = rospy.ServiceProxy('stereo_wall_detection/detect_wall', DetectWall)
     self.server = actionlib.simple_action_server.SimpleActionServer(self.name, DetectWallNormAction, self.execute_cb)
 
     if(not self.sim):
-      self.projector_client = dynamic_reconfigure.client.Client('camera_synchronizer_node') 
+      self.projector_client = dynamic_reconfigure.client.Client('camera_synchronizer_node')
       self.projector_on = {'narrow_stereo_trig_mode': 3}
       self.projector_off = {'narrow_stereo_trig_mode': 4}
       self.projector_sub = rospy.Subscriber("projector_controller/rising_edge_timestamps", roslib.msg.Header, self.projector_cb)
-    
+
 
 
   def execute_cb(self, goal):
- 
-    #point head at wall 
+
+    #point head at wall
     point_head_goal = PointHeadGoal()
     point_head_goal.target.header.frame_id = goal.look_point.header.frame_id
     point_head_goal.target.point.x = goal.look_point.point.x
@@ -89,13 +89,13 @@ class DetectWallNormServer:
       return
 
     # turn on projector
-    if(not self.sim): 
+    if(not self.sim):
       rospy.loginfo('turn on projector')
-      self.projector_ready = False    
+      self.projector_ready = False
       self.projector_client.update_configuration(self.projector_on)
       while(not self.projector_ready):
-        rospy.sleep(0.01)  
-  
+        rospy.sleep(0.01)
+
     # detect wall norm
     try:
       wall = self.detect_wall_srv(DetectWallRequest())
@@ -116,7 +116,7 @@ class DetectWallNormServer:
     result.wall_point = wall.wall_point
     self.server.set_succeeded(result)
 
-  def projector_cb(self, data): 
+  def projector_cb(self, data):
     if(not self.server.is_active()):
       self.projector_ready = False
       return
@@ -125,7 +125,7 @@ class DetectWallNormServer:
 
 
 if __name__ == '__main__':
-    
+
   # Initializes a rospy node so that the SimpleActionClient can
   # publish and subscribe over ROS.
   name ='detect_wall_norm'
