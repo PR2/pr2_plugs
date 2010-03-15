ef4474c0494bc12edf0453c82f0c2841cc3bc4b2
eitan <devnull@localhost>
2010-03-15 22:08:49 +0000

    pr2_plugs_actions: added load_plug_description node

---
+++ pr2_plugs_actions/scripts/load_plug_description.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+PKG = 'pr2_plugs_actions'
+import roslib; roslib.load_manifest(PKG)
+import rospy
+import os
+
+if __name__ == '__main__':
+  rospy.init_node('load_plug_description', anonymous=True)
+
+  import os
+
+  plug_desc_path = None
+  if 'ROBOT_NAME' not in os.environ:
+    rospy.logwarn('ROBOT_NAME environment variable not set.  Using generic plug description configuration')
+  else:
+    filename = os.environ['ROBOT_NAME'] + '_plug_description.yaml'
+    plug_desc_path = roslib.packages.find_resource(PKG, filename)
+    if not plug_desc_path:
+      rospy.logwarn('Could not find robot specific plug description configuration file "%s" in %s' % (filename, PKG))
+
+  if not plug_desc_path:
+    plug_desc_path = roslib.packages.find_resource(PKG, 'pr2_plug_description.yaml')
+    if not plug_desc_path:
+      rospy.logfatal('Could not find plug description configuration file in %s' % PKG)
+      sys.exit(1)
+
+  plug_desc_path = plug_desc_path[0]
+  rospy.loginfo('Loading plug description from %s' % plug_desc_path)
+
+  rospy.spin()
