9fbcb5c19ecd877e4391cdc11d6d0763595751c5
Wim Meeussen <wim@willowgarage.com>
2012-02-15 13:22:18 -0800

    stay subscribed to cloud messages

--- stereo_wall_detection/src/wall_extractor_pcl.cpp
+++ stereo_wall_detection/src/wall_extractor_pcl.cpp
@@ -51,6 +51,9 @@
 #include <pcl/io/io.h>
 #include <pcl/features/feature.h>
 #include <pcl/point_types.h>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
+#include <boost/bind.hpp>
 
 #include <visualization_msgs/Marker.h>
 
@@ -61,6 +64,10 @@ public:
   {
     serv_ = nh_.advertiseService ("detect_wall", &PlanarFit::detect_wall, this);
 
+    // subscribe to pointcloud messages from stereo camera
+    //cloud_sub_ = nh_.subscribe("points2", 10, boost::bind(&PlanarFit::cloudCb, this, _1));  // what's wrong with this line???
+    cloud_sub_ = nh_.subscribe("points2", 10, &PlanarFit::cloudCb, this);
+
     // advertise selected points from wall detection algorithm
     plane_points_ = nh_.advertise<sensor_msgs::PointCloud2>("wall_points", 10);
     plane_marker_ = nh_.advertise<visualization_msgs::Marker>("wall_marker", 10);
@@ -68,16 +75,44 @@ public:
   
   virtual ~PlanarFit () { }
   
+
+
+  //void cloudCb(const sensor_msgs::PointCloud2ConstPtr& cloud)
+  void cloudCb(const boost::shared_ptr<const sensor_msgs::PointCloud2>& cloud)
+  {
+    boost::mutex::scoped_lock lock(cloud_mutex_);
+    cloud_msg_ = cloud;
+    cloud_condition_.notify_all();
+  }
+
+
+
   bool detect_wall (stereo_wall_detection::DetectWall::Request &req, stereo_wall_detection::DetectWall::Response &resp)
   {
-    ros::Duration(10).sleep();
-    // get pointcloud
-    sensor_msgs::PointCloud2ConstPtr cloud_msg = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("points2", nh_, ros::Duration(10.0));
-    if (!cloud_msg){
-      ROS_ERROR("Did not receive a pointcloud in 10 seconds");
-      return false;
+    ros::Time start_time = ros::Time::now();
+
+    // wait for new pointcloud
+    boost::mutex::scoped_lock lock(cloud_mutex_);
+    bool cloud_found = false;
+    while (ros::ok() && !cloud_found){
+      // check for timeout
+      if (ros::Time::now() > start_time + ros::Duration(10.0)){
+        ROS_ERROR("Did not receive a pointcloud in 10 seconds");
+        return false;
+      }
+
+      // check if this is a good pointcloud
+      if (cloud_msg_->header.stamp < start_time){
+        // TODO: count number of points in cloud
+        cloud_found = true;
+      }
+
+      // wait for another cloud
+      if (!cloud_found)     
+        cloud_condition_.wait(lock);
     }
     ROS_INFO("point cloud received");
+
 
     double threshold;
     nh_.param<double>("distance_threshold", threshold, 0.01);
@@ -90,7 +125,7 @@ public:
 
     // find plane in pointcloud
     pcl::PointCloud<pcl::PointXYZ> cloud;
-    pcl::fromROSMsg(*cloud_msg, cloud);
+    pcl::fromROSMsg(*cloud_msg_, cloud);
     pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
     pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
     pcl::SACSegmentation<pcl::PointXYZ> seg;
@@ -131,19 +166,19 @@ public:
     if( normal.z < 0 ) {
       // Normal points towards camera; reverse it
       ROS_WARN("Wall Normal points towards camera frame: %s", 
-        cloud_msg->header.frame_id.c_str());
+        cloud_msg_->header.frame_id.c_str());
       normal.x = -normal.x;
       normal.y = -normal.y;
       normal.z = -normal.z;
     } else {
       ROS_WARN("Wall normal points away from camera frame: %s", 
-        cloud_msg->header.frame_id.c_str());
+        cloud_msg_->header.frame_id.c_str());
     }
 
-    resp.wall_norm.header = cloud_msg->header;
+    resp.wall_norm.header = cloud_msg_->header;
     resp.wall_norm.vector = normal;
 
-    resp.wall_point.header = cloud_msg->header;
+    resp.wall_point.header = cloud_msg_->header;
     resp.wall_point.point.x = centroid[0];
     resp.wall_point.point.y = centroid[1];
     resp.wall_point.point.z = centroid[2];
@@ -154,7 +189,7 @@ public:
       visualization_msgs::Marker marker;
 
       // set up marker
-      marker.header = cloud_msg->header;
+      marker.header = cloud_msg_->header;
       marker.id = 0;
       marker.type =   visualization_msgs::Marker::ARROW;
       marker.action = visualization_msgs::Marker::ADD; // add/modify
@@ -183,7 +218,7 @@ public:
     ROS_INFO("Found wall at %f %f %f with normal: %f %f %f in frame %s",
              resp.wall_point.point.x, resp.wall_point.point.y, resp.wall_point.point.z,
              resp.wall_norm.vector.x, resp.wall_norm.vector.y, resp.wall_norm.vector.z, 
-             cloud_msg->header.frame_id.c_str());
+             cloud_msg_->header.frame_id.c_str());
 
     return true;
   }
@@ -193,6 +228,11 @@ private:
   ros::ServiceServer serv_;
   ros::Publisher plane_points_;
   ros::Publisher plane_marker_;
+  ros::Subscriber cloud_sub_;
+
+  sensor_msgs::PointCloud2ConstPtr cloud_msg_;
+  boost::condition cloud_condition_;
+  boost::mutex cloud_mutex_;
 };
 
 
@@ -202,7 +242,8 @@ int main (int argc, char** argv)
   ros::init (argc, argv, "wall_extractor");
 
   PlanarFit p;
-  ros::spin();
+  ros::MultiThreadedSpinner spinner(2);  // extra thread so we can receive cloud messages while in the service call
+  spinner.spin();
 
   return (0);
 }
