dca0dabac88a2ff489bc5fcea2a9ff71af80fd5a
TheDash <noobaca2@gmail.com>
2014-07-21 12:53:18 -0400

    Exported libraries

--- visual_pose_estimation/include/visual_pose_estimation/planar.h
+++ visual_pose_estimation/include/visual_pose_estimation/planar.h
@@ -1,7 +1,16 @@
 #ifndef VISUAL_POSE_ESTIMATION_PLANAR_H
 #define VISUAL_POSE_ESTIMATION_PLANAR_H
 
-#include <cv.h>
+//#include <cv.h>
+#include "opencv2/core/core_c.h"
+#include "opencv2/core/types_c.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/legacy/legacy.hpp"
+#include "opencv2/legacy/compat.hpp"
+#include <opencv2/nonfree/nonfree.hpp>
 
 namespace cv {
 
